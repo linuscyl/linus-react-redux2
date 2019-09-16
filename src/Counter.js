fe31381f4b973195276f98d8429f8f92bae81e38
Linus <linuschan@joyaether.com>
2019-09-16 17:43:26 +0800

    testing

---
+++ src/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Counter(props) {
+
+  console.log('props', props)
+  return (
+    <div>
+      <h1>I am a counter</h1>
+      <p> Count: {props.count}</p>
+      <button onClick={props.onIncrementClick}>increment</button>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  console.log('mapStateToProps_state:', state);
+  return {
+    count: state.count
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncrementClick: () => {
+      console.log('onClicked');
+      const action = { type: 'INCREMENT' };
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
